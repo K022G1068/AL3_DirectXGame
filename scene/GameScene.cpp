419a7717a4f66932214aeb6c648d1f2d1f74945c
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2023-04-17 15:02:10 +0900

    Update

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,11 +1,17 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include"ImGuiManager.h"
+#include"PrimitiveDrawer.h"
+#include"AxisIndicator.h"
+
 
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
 	delete sprite_;
+	delete model_;
+	delete debugCamera_;
 }
 
 void GameScene::Initialize() {
@@ -15,6 +21,19 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	textureHandle_ = TextureManager::Load("sample.png");
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	audioHandle_ = audio_->LoadWave("fanfare.wav");
+	//audio_->PlayWave(audioHandle_);
+	voiceHandle_ = audio_->PlayWave(audioHandle_, true);
+
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
+
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
 void GameScene::Update() 
@@ -23,7 +42,18 @@ void GameScene::Update()
 	position.x += 1.0f;
 	position.y += 1.0f;
 
+	debugCamera_->Update();
+
 	sprite_->SetPosition(position);
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(voiceHandle_);
+	}
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+	ImGui::ShowDemoWindow();
 }
 
 void GameScene::Draw() {
@@ -52,7 +82,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -65,6 +97,7 @@ void GameScene::Draw() {
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
 	sprite_->Draw();
+	
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include"DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -47,7 +48,16 @@ private: // メンバ変数
 
 	//Texture
 	uint32_t textureHandle_ = 0;
+	uint32_t audioHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
 	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+
+	float inputFloat3[3] = {0, 0, 0};
+
+	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
